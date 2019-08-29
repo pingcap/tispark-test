2dd78225dc844fbd7568949e4ff00601d7d5a833
zhaoyixuan <zhaoyixuan@polomi.com>
2019-08-29 10:43:17 +0800

    fix mvn command error

--- tispark-regression-test-daily/daily_test.groovy
+++ tispark-regression-test-daily/daily_test.groovy
@@ -144,7 +144,7 @@ def call(ghprbCommentBody) {
                     sh """
                         export MAVEN_OPTS="-Xmx6G -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=51M"
                         mvn compile ${MVN_PROFILE}
-                        { time mvn --debug test ${MVN_PROFILE} -Dtest=moo ${mvnStr} ; } 2 > mvn.log
+                        { time mvn --debug test ${MVN_PROFILE} -Dtest=moo ${mvnStr} ; } 2> mvn.log
                     """
                 }
             }
