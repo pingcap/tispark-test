86d81d339783d67cfb0f8a8bf3239644d5e275d5
marsishandsome <marsishandsome@gmail.com>
2020-11-26 12:33:26 +0800

    update idleMinutes to 10

--- tispark-regression-test-daily/daily_test.groovy
+++ tispark-regression-test-daily/daily_test.groovy
@@ -111,7 +111,7 @@ def call(ghprbCommentBody, branch, notify) {
 
     def label = "regression-test-tispark"
 
-    podTemplate(name: label, label: label, instanceCap: 10, idleMinutes: 720, containers: [
+    podTemplate(name: label, label: label, instanceCap: 10, idleMinutes: 10, containers: [
             containerTemplate(name: 'golang', image: 'hub.pingcap.net/jenkins/centos7_golang-1.12:cached',
                     envVars: [
                             envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375'),
